d93d67bdabf99c5d4934d7bd073d70d335dadb35
brainzaps <skrypak.cookiedev@gmail.com>
2024-03-10 23:37:40 +0200

    feat: add some args

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
-use camino::Utf8PathBuf;
-use clap::Parser;
-use serde::Serialize;
-use std::fmt;
+use clap::{command, Arg};
 
-/// Scaffold a new post for your blog
-#[derive(Parser, Debug)]
-#[clap(version)]
-struct Args {
-    #[clap(short, long, default_value_t, value_enum)]
-    rit: Command,
-
-    #[clap(short, long, default_value = "content")]
-    output_dir: Utf8PathBuf,
-}
 fn main() {
-    let args = Args::parse();
-    dbg!(&args);
+    let match_result = command!()
+        .arg(
+            Arg::new("firstname")
+                .short('f')
+                .alias("fname")
+                .required(true)
+                .help("The person's first name")
+                .conflicts_with("fluffy"),
+        )
+        .arg(
+            Arg::new("lastname")
+                .short('l')
+                .alias("lname")
+                .required(true)
+                .help("The person's last name"),
+        )
+        .arg(Arg::new("fluffy").long("fluffy"))
+        .get_matches();
 
-    print!("{}", args.rit);
-}
-
-#[derive(clap::ValueEnum, Clone, Default, Debug, Serialize)]
-#[serde(rename_all = "kebab-case")]
-enum Command {
-    #[default]
-    Init,
-}
-
-impl fmt::Display for Command {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Command::Init => write!(f, "INIT"),
-        }
-    }
+    print!("{:?}", match_result);
 }
